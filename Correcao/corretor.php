12997641d549d203c84056a8e0f1f3a62cde0951
luiz787 <diasbertoluiz@gmail.com>
2016-10-17 11:32:14 -0200

    Melhorias nos corretores PHP/JAVA e adição de layout WEB

--- Correcao/corretor.php
+++ Correcao/corretor.php
@@ -1,26 +1,40 @@
 <?php
-
-$respostas = array("Quinze","Sessenta e quatro");
-$numeroCorretas = 0;
-if (file_exists('ModeloProva.xml')) {
-    $xml = simplexml_load_file('ModeloProva.xml');
-    //print_r($xml);
-} else {
-    exit('Failed to open ModeloProva.xml.');
+$caminhoProva = $_FILES['prova']['tmp_name'];
+$caminhoGabarito = $_FILES['gabarito']['tmp_name'];
+function lerProva($caminhoprova){
+	if (file_exists($caminhoprova)) {
+ 	   $xml = simplexml_load_file($caminhoprova);
+	} else {
+	    exit('Falha ao abrir ModeloProva.xml.');
+	}
+	return $xml;
 }
-$questao_length=count($xml->questao);
-for ($i=0; $i<$questao_length; $i++){
-	$questaoAtual = $xml->questao[$i];
-	$todasAlternativas = $questaoAtual->alternativa;
-	foreach ($todasAlternativas as $alt){
-		if ((string)$alt['correta']=="true"){
+function lerGabarito($caminhogabarito){
+	$a = file($caminhogabarito);
+	$respostasConcatenadas = implode(",", $a);
+	$respostas = explode(",", $respostasConcatenadas);
+	foreach ($respostas as &$key) {
+		$key = substr($key, 3);
+		$key = rtrim($key);
+	}
+	return $respostas;
+}
+function corrigir($xml, $respostas){
+	$numeroCorretas=0;
+	$questao_length=count($xml->questao);
+	for ($i=0; $i<$questao_length; $i++){
+		$questaoAtual = $xml->questao[$i];
+		$todasAlternativas = $questaoAtual->alternativa;
+		foreach ($todasAlternativas as $alt){
+			if ((string)$alt['correta']=="true"){
 				if ($respostas[$i]==$alt){
 					$numeroCorretas++;
 				}
+			}
 		}
 	}
+	return $numeroCorretas;
 }
-
 echo "
 <!DOCTYPE html>
 <html>
@@ -28,5 +42,5 @@ echo "
 	<title>Correção</title>
 	<meta charset=\"utf-8\">
 </head>
-<body><p>Questões acertadas: ".$numeroCorretas."</p></body></html>"
+<body><p>Questões acertadas:".corrigir(lerProva($caminhoProva), lerGabarito($caminhoGabarito))."</p></body></html>"
 ?>
