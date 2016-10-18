4e593e246ba67bf217af358e62a896e5a29bab3b
luiz787 <diasbertoluiz@gmail.com>
2016-10-18 21:23:03 -0200

    Correção de bugs e adição de arquivo TXT teste de gabarito

--- Correcao/Correcao.java
+++ Correcao/Correcao.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
@@ -17,34 +18,77 @@ import org.w3c.dom.Element;
 public class Correcao {
     /**
      * @param args the command line arguments
+     * @throws java.lang.Exception
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         JFileChooser seletor = new JFileChooser();
-        String respostasConcatenadas = null;
-        String respostas[] = null;
+        String respostasConcatenadas=null;
         FileNameExtensionFilter filtroXML = 
                 new FileNameExtensionFilter("Extensible Markup Language", "xml");
         seletor.setFileFilter(filtroXML);
+        JOptionPane.showMessageDialog(null, "Insira o arquivo da PROVA XML");
         seletor.showOpenDialog(seletor);
         File arquivoProva = 
         seletor.getSelectedFile();
+        if (validarExtensão(arquivoProva.getName(), "xml")==false){
+           throw new Exception("Erro na extensão do arquivo");
+        }
         FileNameExtensionFilter filtroTXT = 
                 new FileNameExtensionFilter("Arquivo de texto", "txt");
         seletor.setFileFilter(filtroTXT);
+        JOptionPane.showMessageDialog(null, "Insira o arquivo do GABARITO TXT");
         seletor.showOpenDialog(seletor);
         File arquivoGabarito = seletor.getSelectedFile();
+        if (validarExtensão(arquivoGabarito.getName(), "txt")==false){
+           throw new Exception("Erro na extensão do arquivo");
+        }
         try {
             respostasConcatenadas = readFile(arquivoGabarito);
-            respostas = respostasConcatenadas.split("\n");
-            for (int i=0; i<respostas.length; i++) {
-                respostas[i]=respostas[i].substring(3);
-            }
         } catch (IOException ex) {
             Logger.getLogger(Correcao.class.getName()).log(Level.SEVERE, null, ex);
         }
-        System.out.println(corrigirProva(arquivoProva, respostas));
+        JOptionPane.showMessageDialog(null, "Questões acertadas: "+
+            corrigirProva(arquivoProva, formatarRespostas(respostasConcatenadas)));
     }
-
+    /**
+    * Recebe uma string lida de um arquivo TXT de gabarito e a formata 
+    * alocando-a em um vetor de strings.
+    * @param respostasConcatenadas string lida do arquivo TXT
+    * @return vetor de string das respostas
+    */
+    public static String[] formatarRespostas (String respostasConcatenadas) {
+        String respostas[]=null;
+        respostas = respostasConcatenadas.split("\n");
+        for (int i=0; i<respostas.length; i++) {
+            if (i!=respostas.length-1){
+               respostas[i]=respostas[i].substring(3, respostas[i].length()-1);
+               //remove caracter indesejado ocorrente em todas as linhas, em exceção da última.
+            } else {
+               respostas[i]=respostas[i].substring(3);
+            }
+        }
+        return respostas;
+    }
+    /**
+     * Compara a extensão de um arquivo com a extensão válida requerida, e 
+     * retorna true caso a extensão do arquivo for válida
+     * @param nomeDoArquivo nome do arquivo a ser validado
+     * @param extensãoVálida extensão na qual o arquivo deve ser
+     * @return true caso o arquivo seja da extensão válida, false caso contrário
+     */
+    public static boolean validarExtensão (String nomeDoArquivo, String extensãoVálida){
+        String extArquivo = nomeDoArquivo.substring(nomeDoArquivo.lastIndexOf(".")+1, nomeDoArquivo.length());
+        String extVálida = extensãoVálida;
+        return extVálida.equals(extArquivo);
+        
+    }
+    /**
+     * Lê um arquivo texto de um gabarito e passa as respostas para uma única 
+     * string.
+     * @param gabarito Arquivo do gabarito
+     * @return String com todas as respostas concatenadas.
+     * @throws IOException 
+     */
     public static String readFile(File gabarito) throws IOException {
         String content = null;
         File file = gabarito;
@@ -133,10 +177,6 @@ public class Correcao {
                     for (int i = 0; i < eElement.getElementsByTagName("alternativa").getLength(); i++) {
                         if ("true".equals(eElement.getElementsByTagName("alternativa").item(i).getAttributes().item(0).getTextContent())) {
                             altCorretaCompleta = eElement.getElementsByTagName("alternativa").item(i).getTextContent();
-                            /*System.out.println(x[temp]);
-                            System.out.println(altCorretaCompleta);
-                            System.out.println("debug");*/
-                            //bug na comparação
                             if (x[temp].equals(altCorretaCompleta)){
                                 respostasCorretas++;
                             }
