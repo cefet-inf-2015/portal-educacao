ccd1b50704805aa9bd790e24858fbe3326c5aee9
Haddadson <gabriel.haddad15@gmail.com>
2016-10-31 11:21:57 -0200

    Posts vazios não aparecem mais
    Agora não é mais possível adicionar posts sem conteúdo na timeline

--- Mural/web/UploadBD.php
+++ Mural/web/UploadBD.php
@@ -1,10 +1,10 @@
 <?php
 	$conexao = new mysqli('localhost', 'root', '', 'Mural') or die("Erro na conexão");
 	$textoPostagem = htmlentities($_GET['post']);
-	$imagem = htmlentities($_FILES['arq']);
+	//$imagem = htmlentities($_FILES['arq']);
 	$dataPostagem = date("d-m-Y H:i:s");
-	
-	$query = "INSERT INTO Mural (data, conteudo, arquivo) VALUES ($dataPostagem, $textoPostagem, $arquivo)";
+
+	$query = "INSERT INTO Mural (data, conteudo) VALUES ($dataPostagem, $textoPostagem)";
 	$conexao ->query($query);
 	
 ?>
