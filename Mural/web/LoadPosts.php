abc5840610b12f951dcf0f8bd3cf24c5873b359d
Haddadson <gabriel.haddad15@gmail.com>
2016-11-21 11:26:25 -0200

    leitura do bd iniciado

---
+++ Mural/web/LoadPosts.php
@@ -0,0 +1,23 @@
+<?php
+
+$linha = $_GET['linha'];
+
+$dados = array();
+
+$servidor = 'localhost';
+$usuario = 'root';
+$senha = '';
+$bd = 'mural';
+
+$mysqli = new mysqli($servidor, $usuario, $senha, $bd);
+
+
+$load= $mysqli->query("SELECT conteudo, data, usuario FROM mural");
+
+for($i = 0; $i< $linha; $i++){
+	$dados = $load->fetch_assoc();
+}
+
+echo json_encode($dados);
+
+?>
